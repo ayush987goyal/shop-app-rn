64434a6374a38ec809c034a0a8f60f9037104eed
Ayush Goyal <goyalayu@amazon.com>
2020-07-11 13:57:35 +0530

    Add produc tdetails screen

--- src/screens/shop/ProductDetailScreen.tsx
+++ src/screens/shop/ProductDetailScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useLayoutEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Image, Text, Button, View } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { ProductsStackParamsList } from '../../navigation/ShopNavigator';
+import Colors from '../../constants/Colors';
 
 interface ProductDetailScreenProps {
   navigation: StackNavigationProp<ProductsStackParamsList, 'ProductDetail'>;
@@ -20,13 +21,39 @@ const ProductDetailScreen: React.FC<ProductDetailScreenProps> = ({ navigation, r
   }, [navigation, selectedProduct.title]);
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-      <Text>{selectedProduct.description}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+
+      <View style={styles.actions}>
+        <Button title="Add to Cart" color={Colors.primary} onPress={() => {}} />
+      </View>
+
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20,
+  },
+});
 
 export default ProductDetailScreen;
