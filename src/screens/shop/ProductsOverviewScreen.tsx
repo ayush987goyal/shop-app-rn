83df72ec71c2102cfea0ac26ac78e809dac34ac0
Ayush Goyal <goyalayu@amazon.com>
2020-07-11 13:10:30 +0530

    Add productItem and usage

--- src/screens/shop/ProductsOverviewScreen.tsx
+++ src/screens/shop/ProductsOverviewScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { RootState } from '../../store';
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverviewScreen = () => {
   const products = useSelector((state: RootState) => state.products.availableProducts);
@@ -11,7 +12,9 @@ const ProductsOverviewScreen = () => {
     <FlatList
       data={products}
       keyExtractor={item => item.id}
-      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+      renderItem={itemData => (
+        <ProductItem product={itemData.item} onViewDetail={() => {}} onAddToCart={() => {}} />
+      )}
     />
   );
 };
