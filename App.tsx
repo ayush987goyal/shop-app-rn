229642589dcf5c81062f8ae5850fd3b62100c73a
Ayush Goyal <goyalayu@amazon.com>
2020-07-11 14:04:09 +0530

    Add custom font and use

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Font from 'expo-font';
 
 import store from './src/store';
 import ShopNavigator from './src/navigation/ShopNavigator';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (!fontsLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontsLoaded(true)} />;
+  }
+
   return (
     <Provider store={store}>
       <NavigationContainer>
